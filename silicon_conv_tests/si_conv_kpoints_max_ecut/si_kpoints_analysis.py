616400b15d3abc81b9aedf46f926cd5eb39db035
tsievers <tsievers@physnet.uni-hamburg.de>
2022-02-04 13:44:15 +0100

    Si convergence test over MP-grid with maximal Ecut

---
+++ silicon_conv_tests/si_conv_kpoints_max_ecut/si_kpoints_analysis.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+files = os.listdir("out_files")
+data = np.zeros((2, len(files)))
+
+print(files)
+
+for i, file in enumerate(files):
+    data[0, i] = file.split("_")[2].split(".")[0]
+    with open("out_files/" + file, "r") as f:
+        searchlines = f.readlines()
+    for line in searchlines:
+        if "!    total energy" in line:
+            data[1, i] = line.split(" ")[-2]
+
+print(data)
+
+plt.plot(data[0], data[1])
+
+plt.xlabel("N (NxNxN Monkhorst-Pack grid)")
+plt.ylabel("total energy [Ry]")
+
+plt.savefig("si_kpoints.pdf", bbox_inches="tight")
+
+convergence_crit = 1e-3
+
+converged_index = np.argmax(abs(np.ediff1d(data[1]))<convergence_crit)
+print("Calculation is converged with N = ", data[0][converged_index])
