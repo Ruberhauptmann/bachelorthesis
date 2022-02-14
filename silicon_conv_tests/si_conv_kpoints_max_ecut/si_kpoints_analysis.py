49772c41110f758b9164bf5d851d69b5f9fee847
tsievers <tsievers@physnet.uni-hamburg.de>
2022-02-14 11:11:40 +0100

    Remade convergence tests

--- silicon_conv_tests/si_conv_kpoints_max_ecut/si_kpoints_analysis.py
+++ silicon_conv_tests/si_conv_kpoints_max_ecut/si_kpoints_analysis.py
@@ -1,25 +1,22 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import re
 
 
 files = os.listdir("out_files")
-data = np.zeros((2, len(files)))
-
-print(files)
+k_points = np.zeros(len(files))
+e_tot = np.zeros(len(files))
 
 for i, file in enumerate(files):
-    data[0, i] = file.split("_")[2].split(".")[0]
+    k_points[i] = re.search("[0-9]+", file).group(0)
     with open("out_files/" + file, "r") as f:
         searchlines = f.readlines()
     for line in searchlines:
         if "!    total energy" in line:
-            data[1, i] = line.split(" ")[-2]
+            e_tot[i] = re.search("-\d+.\d+", line).group(0)
 
-data = np.array([data[0, data.argsort()[0]], data[1, data.argsort()[0]]])
-print(data)
-
-plt.plot(data[0], data[1], marker='o', linestyle='dashed')
+plt.plot(k_points, e_tot, marker='o', linestyle='dashed')
 
 plt.xlabel("N (NxNxN Monkhorst-Pack grid)")
 plt.ylabel("total energy [Ry]")
@@ -28,5 +25,6 @@ plt.savefig("si_kpoints.pdf", bbox_inches="tight")
 
 convergence_crit = 1e-3
 
-converged_index = np.argmax(abs(np.ediff1d(data[1]))<convergence_crit)
-print("Calculation is converged with N = ", data[0][converged_index])
+converged_index = np.argmax(abs(np.ediff1d(e_tot))<convergence_crit)
+print("Calculation is converged with N = ", k_points[converged_index])
+
