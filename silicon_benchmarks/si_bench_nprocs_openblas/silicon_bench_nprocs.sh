4de83a62e2aaadf2e67bc3e6f15d1e35ca49bee7
tsievers <tsievers@physnet.uni-hamburg.de>
2022-02-24 11:22:47 +0100

    Silicon first testruns with mkl/openblas

---
+++ silicon_benchmarks/si_bench_nprocs_openblas/silicon_bench_nprocs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -q lernfix.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o $HOME/job_logs/silicon/bench_nprocs_openblas/
+#$ -l h_cpu=01:00:00
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0-openblas
+
+module load openmpi/4.1.0.gcc10.2-infiniband
+module load openblas/0.3.20
+
+mpirun $QEPREFIX/bin/pw.x -i in_files/si_bench_$1.scf
+
