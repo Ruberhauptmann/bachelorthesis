1f4907687c96c317ffa74887f7f97cde776b830f
tsievers <tsievers@physnet.uni-hamburg.de>
2022-02-25 08:26:10 +0100

    Sent TaS2 cdw nk benchmarks

---
+++ TaS2_benchmarks/TaS2_bench_nk/cdw/job_files/TaS2_cdw_bench_nk_3_n_procs_39.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -q lernfix.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/TaS2/bench_nk/cdw/nk_3
+#$ -l h_cpu=60:00:00
+#$ -pe mpi 39
+#$ -N TaS2_cdw_bench_nk_3_n_procs_39
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0
+
+module load openmpi/4.1.0.gcc10.2-infiniband
+
+mpirun $QEPREFIX/bin/pw.x -nk 3 -i in_files/TaS2_cdw_bench_nk_3_n_procs_39.scf
---
+++ TaS2_benchmarks/TaS2_bench_nk/cdw/job_files/TaS2_cdw_bench_nk_8_n_procs_32.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -q lernfix.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/TaS2/bench_nk/cdw/nk_8
+#$ -l h_cpu=60:00:00
+#$ -pe mpi 32
+#$ -N TaS2_cdw_bench_nk_8_n_procs_32
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0
+
+module load openmpi/4.1.0.gcc10.2-infiniband
+
+mpirun $QEPREFIX/bin/pw.x -nk 8 -i in_files/TaS2_cdw_bench_nk_8_n_procs_32.scf
