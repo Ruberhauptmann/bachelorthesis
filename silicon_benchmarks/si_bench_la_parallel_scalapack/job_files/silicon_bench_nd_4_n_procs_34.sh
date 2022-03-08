e9c4d8d725fb4ad9574fd04513eb040b0f4ad190
tsievers <tsievers@physnet.uni-hamburg.de>
2022-03-08 16:43:45 +0100

    Inputs for parallel Linear Algebra benchmarks

---
+++ silicon_benchmarks/si_bench_la_parallel_scalapack/job_files/silicon_bench_nd_4_n_procs_34.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -q lernfix.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/silicon/bench_scalapack_parallel/nd_4
+#$ -l h_cpu=01:00:00
+#$ -pe mpi 34
+#$ -N silicon_bench_nd_4_n_procs_34
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0-scalapack
+
+module load scalapack/2.2.0
+
+/usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/pw.x -nd 4 -i in_files/silicon_bench_nd_4_n_procs_34.scf
---
+++ silicon_benchmarks/si_bench_la_parallel_scalapack/job_files/silicon_bench_nd_9_n_procs_24.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -q lernfix.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/silicon/bench_scalapack_parallel/nd_9
+#$ -l h_cpu=01:00:00
+#$ -pe mpi 24
+#$ -N silicon_bench_nd_9_n_procs_24
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0-scalapack
+
+module load scalapack/2.2.0
+
+/usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/pw.x -nd 9 -i in_files/silicon_bench_nd_9_n_procs_24.scf
