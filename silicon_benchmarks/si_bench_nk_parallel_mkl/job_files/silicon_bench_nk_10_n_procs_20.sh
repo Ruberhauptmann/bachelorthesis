b96e53b18d80c6f618062aff936e8fbb49219685
tsievers <tsievers@physnet.uni-hamburg.de>
2022-03-14 12:32:03 +0100

    Silicon parallel mkl benchmark

---
+++ silicon_benchmarks/si_bench_nk_parallel_mkl/job_files/silicon_bench_nk_10_n_procs_20.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -q lernfix.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/silicon/bench_nk_parallel_mkl/nd_4/nk_10
+#$ -l h_cpu=01:00:00
+#$ -pe mpi 20
+#$ -N silicon_bench_nk_10_n_procs_20
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0-mkl
+
+module load intel/mkl/2021.04
+module load openmpi/4.1.0.gcc10.2-infiniband
+
+/usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/pw.x -nd 4 -nk 10 -i in_files/silicon_bench_nk_10_n_procs_20.scf
