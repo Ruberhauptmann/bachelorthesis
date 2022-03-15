aba6e21f4567b1c53f8f72b743ad5398524aada4
tsievers <tsievers@physnet.uni-hamburg.de>
2022-03-15 17:23:52 +0100

    Silicon benchmark scalapack with right jobfile

--- silicon_benchmarks/si_bench_nprocs_scalapack/silicon_bench_nprocs.sh
+++ silicon_benchmarks/si_bench_nprocs_scalapack/silicon_bench_nprocs.sh
@@ -12,4 +12,6 @@ QEPREFIX=/fastscratch/tsievers/qe-7.0-scalapack
 
 module load scalapack/2.2.0
 
+export LIBRARY_PATH=/opt/sw1/public/scalapack/2.2.0:$LIBRARY_PATH
+
 /usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/pw.x -i in_files/si_bench_$1.scf
