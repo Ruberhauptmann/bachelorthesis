5510829b270fe60194700d3dc9c19e8fe6634dd3
tsievers <tsievers@physnet.uni-hamburg.de>
2022-03-15 17:21:47 +0100

    Silicon benchmark parallel mkl

--- silicon_benchmarks/si_bench_la_parallel_mkl/si_bench_nprocs_analysis.py
+++ silicon_benchmarks/si_bench_la_parallel_mkl/si_bench_nprocs_analysis.py
@@ -1,4 +1,6 @@
+from multiprocessing.context import SpawnProcess
 import os
+from turtle import speed
 import matplotlib
 import numpy as np
 import sys
@@ -50,9 +52,34 @@ if __name__ == "__main__":
     ax1.set_xlabel("Number of processors")
     ax1.set_ylabel("runtime [s]")
 
-    #fig.legend(loc = "lower right", bbox_to_anchor = [0.9, 0.11])
     ax1.legend()
 
     fig.savefig("si_bench_nprocs_absolute.pdf", bbox_inches="tight")
 
     ### Plot speedup
+
+    with open ("out_files_singlecore/" + os.listdir("out_files_singlecore")[0]) as f:
+        searchlines = f.readlines()
+    
+
+    cputime_singlecore, walltime_singlecore = qe_helper.extract_times(searchlines, 1)
+
+    speedup_cpu = {}
+    speedup_wall = {}
+
+    for i, run in enumerate(n_procs):
+        speedup_cpu[run] = cputime_singlecore / cputimes[run]
+        speedup_wall[run] =  walltime_singlecore / walltimes[run]
+
+    fig, ax1 = plt.subplots()
+
+    for i, run in enumerate(n_procs):
+        ax1.plot(n_procs[run], speedup_cpu[run], label=run, marker='o', linestyle='dashed')
+        #ax1.plot(n_procs[run], walltimes[run], label="WALL " + run, marker='o', linestyle='dashed')
+
+    ax1.set_xlabel("Number of processors")
+    ax1.set_ylabel("speedup")
+
+    ax1.legend()
+
+    fig.savefig("si_bench_nprocs_speedup.pdf", bbox_inches="tight")
