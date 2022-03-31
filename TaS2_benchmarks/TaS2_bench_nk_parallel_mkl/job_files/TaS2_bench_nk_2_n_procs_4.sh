e8e5fc0c4f37348c0e717f312c32be1c22243c72
tsievers <tsievers@physnet.uni-hamburg.de>
2022-03-31 11:32:00 +0200

    TaS2 nk benchmark with parallel mkl

---
+++ TaS2_benchmarks/TaS2_bench_nk_parallel_mkl/job_files/TaS2_bench_nk_2_n_procs_4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -q lernfix.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/TaS2/bench_nk_parallel_mkl/nd_4/nk_2
+#$ -l h_cpu=60:00:00
+#$ -l h_vmem=3G
+#$ -pe mpi 4
+#$ -N TaS2_bench_nk_2_n_procs_4
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0-mkl
+
+module load intel/mkl/2021.04
+module load openmpi/4.1.0.gcc10.2-infiniband
+
+/usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/pw.x -nd 4 -nk 2 -i in_files/TaS2_bench_nk_2_n_procs_4.scf
---
+++ TaS2_benchmarks/TaS2_bench_nk_parallel_mkl/si_bench_nprocs_analysis.py
@@ -0,0 +1,32 @@
+import numpy as np
+import sys
+import os
+
+sys.path.append("../../")
+
+from helper import qe_helper
+from helper.plot_helper import nk_plots
+
+if __name__ == "__main__":
+    for nd in os.listdir("out_files"):
+        ### Plot absolute times
+
+        cputimes, walltimes, n_procs = qe_helper.extract_times_nk("out_files/" + nd, multiple_runs=False)
+
+        nk_plots.plot(cputimes, walltimes, n_procs, "TaS2_nd" + nd, "absolute")
+
+        ### Plot speedup
+
+        cputimes_singlecore, walltimes_singlecore = qe_helper.extract_times("out_files_singlecore", multiple_runs=False)[0:2]
+
+        cputime_singlecore = np.mean(cputimes_singlecore)
+        walltime_singlecore = np.mean(walltimes_singlecore)
+
+        speedup_cpu = {}
+        speedup_wall = {}
+
+        for nk in n_procs:
+            speedup_cpu[nk] = cputime_singlecore / cputimes[nk]
+            speedup_wall[nk] = walltime_singlecore / walltimes[nk]
+
+        nk_plots.plot(speedup_cpu, speedup_wall, n_procs, "TaS2_nd" + nd, "speedup")
