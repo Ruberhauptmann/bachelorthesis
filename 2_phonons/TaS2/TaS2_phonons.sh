9fd20ce2869eb07fc0f9c9cbf556d5e0f381e585
tsievers <tsievers@physnet.uni-hamburg.de>
2022-05-20 15:23:51 +0200

    TaS2 phonon job with openmpi

--- 2_phonons/TaS2/TaS2_phonons.sh
+++ 2_phonons/TaS2/TaS2_phonons.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#$ -q th1prio.q
+#$ -q infinix.q
 #$ -e $HOME/job_logs/error_logs/
 #$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/TaS2/phonons
 #$ -l h_cpu=40:00:00
+#$ -l h_vmem=2G
 #$ -pe mpi 200
 #$ -N TaS2_phonons
 #$ -cwd
 #$ -S /bin/bash
 #$ -M tsievers@physnet.uni-hamburg.de -m as
 
-QEPREFIX=/fastscratch/tsievers/qe-7.0-intel-compiler
+#QEPREFIX=/fastscratch/tsievers/qe-7.0-intel-compiler
+QEPREFIX=/fastscratch/tsievers/qe-7.0-scalapack
 
-module load intel/oneAPI-2021.4
+module load scalapack/2.2.0
 
 mpirun $QEPREFIX/bin/pw.x -nk 20 -i TaS2_cdw.scf
 
