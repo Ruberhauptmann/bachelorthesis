29ba0e23781db66d3ad5502f7fc8dbd3bc481257
tsievers <tsievers@physnet.uni-hamburg.de>
2022-05-23 11:52:55 +0200

    Multiple runs for openmpi scf runs

---
+++ 1_scf/TaS2_benchmarks/TaS2_bench_nprocs/job_files/TaS2_bench_ompi_n_procs_6_0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -q th1prio.q
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/TaS2/bench_nprocs/0
+#$ -pe mpi 6
+#$ -N TaS2_bench_ompi_n_procs_6_0
+#$ -l h_cpu=25:00:00
+#$ -l h_vmem=8G
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0
+
+module load openmpi/4.1.0.gcc10.2-infiniband
+
+/usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/pw.x -i in_files/TaS2_bench_ompi_n_procs_6_0.scf
