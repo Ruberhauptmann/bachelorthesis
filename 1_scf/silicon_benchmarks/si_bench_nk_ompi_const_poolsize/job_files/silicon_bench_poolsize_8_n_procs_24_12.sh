0e2c3f0ec71f044c8789d808180771cce45ec340
tsievers <tsievers@physnet.uni-hamburg.de>
2022-05-30 16:41:29 +0200

    silicon ompi nk benchmark multiple runs

---
+++ 1_scf/silicon_benchmarks/si_bench_nk_ompi_const_poolsize/job_files/silicon_bench_poolsize_8_n_procs_24_12.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -q th1prio.q
+#$ -e $HOME/job_logs/error_logs/
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/silicon/bench_nk_ompi_const_poolsize/12/8
+#$ -l h_cpu=01:00:00
+#$ -l h_vmem=1G
+#$ -l excl=TRUE
+#$ -pe mpi 24
+#$ -N silicon_bench_poolsize_8_n_procs_24_12
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0
+
+module load openmpi/4.1.0.gcc10.2-infiniband
+
+/usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/pw.x -nk 3 -i in_files/silicon_bench_poolsize_8_n_procs_24_12.scf
--- 1_scf/silicon_benchmarks/si_bench_nk_ompi_const_poolsize/si_bench_nk_analysis.py
+++ 1_scf/silicon_benchmarks/si_bench_nk_ompi_const_poolsize/si_bench_nk_analysis.py
@@ -5,9 +5,9 @@ from qe_benchmarking import qe_helper, nk_plots
 if __name__ == "__main__":
     ### Plot absolute times
 
-    cputimes, walltimes, n_procs = qe_helper.extract_times_nk("out_files", multiple_runs=False)
+    cputimes, walltimes, n_procs = qe_helper.extract_times_nk("out_files", multiple_runs=True)
 
-    nk_plots.plot(walltimes, n_procs, "si_ompi", "absolute")
+    nk_plots.plot(walltimes, n_procs, "si_ompi", "absolute", plot_error=True)
 
     ### Plot speedup
 
@@ -19,24 +19,30 @@ if __name__ == "__main__":
     speedup = {}
 
     for nk in n_procs:
-        speedup[nk] = walltime_singlecore / walltimes[nk]
+        speedup[nk] = np.zeros(walltimes[nk].shape)
+        for run_index, walltime_nk in enumerate(walltimes[nk]):
+            speedup[nk][run_index] = walltime_singlecore / walltime_nk
 
-    nk_plots.plot(speedup, n_procs, "si_ompi", "speedup")
+    nk_plots.plot(speedup, n_procs, "si_ompi", "speedup", plot_error=True)
 
     ### Plot idle time
 
     wait_time = {}
 
     for nk in n_procs:
-        wait_time[nk] = (walltimes[nk] - cputimes[nk]) / walltimes[nk]
+        wait_time[nk] = np.zeros(walltimes[nk].shape)
+        for run_index, walltime_nk in enumerate(walltimes[nk]):
+            wait_time[nk][run_index] = (walltime_nk - cputimes[nk][run_index]) / walltime_nk
 
-    nk_plots.plot(wait_time, n_procs, "si_ompi", "wait")
+    nk_plots.plot(wait_time, n_procs, "si_ompi", "wait", plot_error=True)
 
     ### Plot efficiency
 
     efficiency = {}
 
     for nk in n_procs:
-        efficiency[nk] = speedup[nk] / n_procs[nk]
+        efficiency[nk] = np.zeros(walltimes[nk].shape)
+        for run_index, walltime_nk in enumerate(walltimes[nk]):
+            efficiency[nk][run_index] = speedup[nk][run_index] / n_procs[nk][run_index]
 
-    nk_plots.plot(efficiency, n_procs, "si_ompi", "efficiency")
+    nk_plots.plot(efficiency, n_procs, "si_ompi", "efficiency", plot_error=True)
