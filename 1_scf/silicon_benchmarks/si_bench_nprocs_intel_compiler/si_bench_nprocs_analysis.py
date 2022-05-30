bcbc0bce338b210d9bdcc1dc91b41079422c9529
tsievers <tsievers@physnet.uni-hamburg.de>
2022-05-30 17:40:47 +0200

    Send jobs for si bench nprocs intel

--- 1_scf/silicon_benchmarks/si_bench_nprocs_intel_compiler/si_bench_nprocs_analysis.py
+++ 1_scf/silicon_benchmarks/si_bench_nprocs_intel_compiler/si_bench_nprocs_analysis.py
@@ -5,28 +5,28 @@ from qe_benchmarking import qe_helper, nprocs_plots
 if __name__ == "__main__":
     ### Plot absolute times
 
-    cputimes, walltimes, n_procs = qe_helper.extract_times("out_files", multiple_runs=False)
+    cputimes, walltimes, n_procs = qe_helper.extract_times("out_files", multiple_runs=True)
 
-    nprocs_plots.plot(walltimes, n_procs, "si_intel", "absolute")
+    nprocs_plots.plot(walltimes, n_procs, "si_intel", "absolute", plot_error=True)
 
     ### Plot speedup
 
-    cputimes_singlecore, walltimes_singlecore = qe_helper.extract_times("out_files_singlecore", multiple_runs=False)[0:2]
+    cputimes_singlecore, walltimes_singlecore = qe_helper.extract_times("out_files_singlecore", multiple_runs=True)[0:2]
 
     walltime_singlecore = np.mean(walltimes_singlecore)
 
     speedup = walltime_singlecore / walltimes
 
-    nprocs_plots.plot(speedup, n_procs, "si_intel", "speedup")
+    nprocs_plots.plot(speedup, n_procs, "si_intel", "speedup", plot_error=True)
 
     ### Plot idle time
 
     wait_time = (walltimes - cputimes) / walltimes
 
-    nprocs_plots.plot(wait_time, n_procs, "si_intel", "wait")
+    nprocs_plots.plot(wait_time, n_procs, "si_intel", "wait", plot_error=True)
 
     ### Plot efficiency
 
     efficiency = speedup / n_procs
 
-    nprocs_plots.plot(efficiency, n_procs, "si_intel", "efficiency")
+    nprocs_plots.plot(efficiency, n_procs, "si_intel", "efficiency", plot_error=True)
---
+++ 1_scf/silicon_benchmarks/si_bench_nprocs_intel_compiler/singlecore_times.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+import glob
+from jinja2 import Environment, FileSystemLoader
+
+if __name__ == "__main__":
+    os.makedirs('in_files_singlecore', exist_ok=True)
+    os.makedirs('job_files_singlecore', exist_ok=True)
+
+    for file in glob.glob('in_files_singlecore/*'):
+        os.remove(file)
+
+    for file in glob.glob('job_files_singlecore/*'):
+        os.remove(file)
+
+    file_loader = FileSystemLoader('templates')
+    env = Environment(loader=file_loader)
+
+    input_template = env.get_template('input.scf.jinja')
+    job_template = env.get_template('silicon_bench_nprocs.sh.jinja')
+
+
+    n_procs = 1
+
+    for i in range(10):
+        job_name = 'silicon_bench_intel_singlecore_' + str(i)
+        prefix = '\'' + job_name +  '\''
+
+        log_path = os.getenv('HOME') + '/job_logs/silicon/bench_nprocs_intel_compiler/singlecore/' + str(i)
+        os.makedirs(log_path, exist_ok=True)
+
+        for file in glob.glob(log_path + '/*'):
+            os.remove(file)
+
+        input_file = input_template.render(prefix=prefix)
+        job_file = job_template.render(n_procs=n_procs,
+        log_path=log_path,
+        job_name=job_name,
+        in_files_path="in_files_singlecore") 
+        with open('in_files_singlecore/' + job_name + '.scf'  , 'w') as fh:
+            fh.write(input_file)
+        with open('job_files_singlecore/' + job_name + '.sh'  , 'w') as fh:
+            fh.write(job_file)
+
+        subprocess.call('qsub job_files_singlecore/' + job_name + '.sh', shell=True)
