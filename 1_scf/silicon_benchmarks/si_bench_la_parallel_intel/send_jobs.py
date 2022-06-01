def45ab6fc41b1de8c07d9effc33ee509ea8aaff
tsievers <tsievers@physnet.uni-hamburg.de>
2022-06-01 22:10:26 +0200

    Si parallel la si multiple runs

--- 1_scf/silicon_benchmarks/si_bench_la_parallel_intel/send_jobs.py
+++ 1_scf/silicon_benchmarks/si_bench_la_parallel_intel/send_jobs.py
@@ -28,49 +28,51 @@ def main():
     job_template = env.get_template('silicon_bench_nd.sh.jinja')
 
     #for nd in find_square_numbers(max_number_procs):
-    for nd in [4, 9, 16, 25]:
-        log_path = os.getenv('HOME') + '/job_logs/silicon/bench_la_parallel_intel/nd_' + str(nd)
+    for run in range(3, 10):
+        for nd in [4, 9, 16, 25]:
+            log_path = os.getenv('HOME') + '/job_logs/silicon/bench_la_parallel_intel/' + str(run) + '/nd_' + str(nd)
+            os.makedirs(log_path, exist_ok=True)
+            for file in glob.glob(log_path + '/*'):
+                os.remove(file)
+
+            for n_procs in range(2, max_number_procs + 1, 2):
+                if n_procs >= nd:
+                    job_name = 'silicon_bench_nd_' + str(nd) + '_n_procs_' + str(n_procs) + '_' + str(run)
+                    prefix = '\'' + job_name +  '\''
+
+                    input_file = input_template.render(prefix=prefix)
+                    job_file = job_template.render(nd=nd, n_procs=n_procs, log_path=log_path, job_name=job_name)
+                    with open('in_files/' + job_name + '.scf'  , 'w') as fh:
+                        fh.write(input_file)
+                    with open('job_files/' + job_name + '.sh'  , 'w') as fh:
+                        fh.write(job_file)
+
+                    #subprocess.call('qsub job_files/' + job_name + '.sh', shell=True)
+
+    input_template = env.get_template('input.scf.jinja')
+    job_template = env.get_template('silicon_bench_nd_auto.sh.jinja')
+
+    for run in range(3, 10):
+        log_path = os.getenv('HOME') + '/job_logs/silicon/bench_la_parallel_intel/' + str(run) + '/nd_auto'
         os.makedirs(log_path, exist_ok=True)
         for file in glob.glob(log_path + '/*'):
             os.remove(file)
 
         for n_procs in range(2, max_number_procs + 1, 2):
-            if n_procs >= nd:
-                job_name = 'silicon_bench_nd_' + str(nd) + '_n_procs_' + str(n_procs)
-                prefix = '\'' + job_name +  '\''
+            job_name = 'silicon_bench_nd_auto_n_procs_' + str(n_procs) + '_' + str(run)
+            prefix = '\'' + job_name +  '\''
 
-                input_file = input_template.render(prefix=prefix)
-                job_file = job_template.render(nd=nd, n_procs=n_procs, log_path=log_path, job_name=job_name)
-                with open('in_files/' + job_name + '.scf'  , 'w') as fh:
-                    fh.write(input_file)
-                with open('job_files/' + job_name + '.sh'  , 'w') as fh:
-                    fh.write(job_file)
+            input_file = input_template.render(prefix=prefix)
+            job_file = job_template.render(nd=nd,
+            n_procs=n_procs,
+            log_path=log_path,
+            job_name=job_name)
+            with open('in_files/' + job_name + '.scf'  , 'w') as fh:
+                fh.write(input_file)
+            with open('job_files/' + job_name + '.sh'  , 'w') as fh:
+                fh.write(job_file)
 
-                subprocess.call('qsub job_files/' + job_name + '.sh', shell=True)
-
-    input_template = env.get_template('input.scf.jinja')
-    job_template = env.get_template('silicon_bench_nd_auto.sh.jinja')
-
-    log_path = os.getenv('HOME') + '/job_logs/silicon/bench_la_parallel_intel/nd_auto'
-    os.makedirs(log_path, exist_ok=True)
-    for file in glob.glob(log_path + '/*'):
-        os.remove(file)
-
-    for n_procs in range(2, max_number_procs + 1, 2):
-        job_name = 'silicon_bench_nd_auto_n_procs_' + str(n_procs)
-        prefix = '\'' + job_name +  '\''
-
-        input_file = input_template.render(prefix=prefix)
-        job_file = job_template.render(nd=nd,
-        n_procs=n_procs,
-        log_path=log_path,
-        job_name=job_name)
-        with open('in_files/' + job_name + '.scf'  , 'w') as fh:
-            fh.write(input_file)
-        with open('job_files/' + job_name + '.sh'  , 'w') as fh:
-            fh.write(job_file)
-
-        subprocess.call('qsub job_files/' + job_name + '.sh', shell=True)
+            subprocess.call('qsub job_files/' + job_name + '.sh', shell=True)
 
 if __name__ == "__main__":
     main()
