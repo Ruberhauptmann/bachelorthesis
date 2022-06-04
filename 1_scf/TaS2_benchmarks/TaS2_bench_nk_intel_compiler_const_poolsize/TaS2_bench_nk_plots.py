f885498eedeab762eee6252ca5b077e7daf2694e
Ruberhauptmann <tjark.sievers@outlook.de>
2022-06-04 20:26:21 +0200

    Ready for sending first draft to Michael

--- 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize/TaS2_bench_nk_plots.py
+++ 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize/TaS2_bench_nk_plots.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         for run_index, walltime_nk in enumerate(walltimes[nk]):
             wait_time[nk][run_index] = (walltime_nk - cputimes[nk][run_index]) / walltime_nk
 
-    nk_plots.plot(wait_time, n_procs, "TaS2_intel", "wait", plot_error=True)
+    nk_plots.plot(wait_time, n_procs, "TaS2_intel", "wait")
 
     ### Plot efficiency
 
@@ -47,4 +47,4 @@ if __name__ == "__main__":
         for run_index, walltime_nk in enumerate(walltimes[nk]):
             efficiency[nk][run_index] = speedup[nk][run_index] / n_procs[nk][run_index]
 
-    nk_plots.plot(efficiency, n_procs, "TaS2_intel", "efficiency", plot_error=True)
+    nk_plots.plot(efficiency, n_procs, "TaS2_intel", "efficiency")
