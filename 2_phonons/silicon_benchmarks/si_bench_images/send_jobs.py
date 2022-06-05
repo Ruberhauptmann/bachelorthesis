f1a6a19662505e33440bb843131a6c8776a507cd
Ruberhauptmann <tjark.sievers@outlook.de>
2022-06-05 14:48:06 +0200

    Plotting script images benchmarks

--- 2_phonons/silicon_benchmarks/si_bench_images/send_jobs.py
+++ 2_phonons/silicon_benchmarks/si_bench_images/send_jobs.py
@@ -31,7 +31,7 @@ def main():
     for run in range(1):
         os.makedirs(job_directory + '/frq/' + str(run), exist_ok=True)
         for nimages in [2, 8, 24]:
-            for n_procs in range(8, max_number_procs+1, 8):
+            for n_procs in range(16, max_number_procs+1, 16):
                 if (n_procs / nimages ) % poolsize == 0:
                     job_name = 'si_ph_bench_nimages_' + str(nimages) + '_n_procs_' + str(n_procs) + '_' + str(run)
 
--- qe_benchmarking_package/qe_benchmarking/ni_plots.py
+++ qe_benchmarking_package/qe_benchmarking/ni_plots.py
@@ -11,6 +11,51 @@ def take_mean(y):
 
     return y_std, y_mean
 
+def take_image_mean(y):
+    y_std = {}
+    y_mean = {}
+
+    for ni in y:
+        y_std[ni] = np.zeros((len(y[ni]), len(y[ni][0])))
+        y_mean[ni] = np.zeros((len(y[ni]), len(y[ni][0])))
+
+    for ni in y:
+        for run_index, run in enumerate(y[ni]):
+            for time_index, times in enumerate(run):
+                y_std[ni][run_index][time_index] = np.std(times, axis=0, ddof=1)
+                y_mean[ni][run_index][time_index] = np.mean(times, axis=0)
+
+    return y_std, y_mean
+
+def take_image_max(y):
+    y_max = {}
+    for ni in y:
+        y_max[ni] = np.zeros((len(y[ni]), len(y[ni][0])))
+        for run_index, run in enumerate(y[ni]):
+            for time_index, times in enumerate(run):
+                y_max[ni][run_index][time_index] = np.max(times)
+
+    return y_max
+
+def plot_images(y, n_procs, prefix):
+    y_images_std, y_images_mean = take_image_mean(y)
+    y_run_std, y_run_mean = take_mean(y_images_mean)
+    for ni in y:
+        y_run_std[ni] = np.mean(y_images_std[ni], axis=0)
+
+    fig, ax1 = plt.subplots()
+
+    for ni in n_procs:
+        ax1.fill_between(n_procs[ni][0], y_run_mean[ni]-y_run_std[ni], y_run_mean[ni]+y_run_std[ni], alpha=0.2)
+        ax1.plot(n_procs[ni][0], y_run_mean[ni], label=ni, marker='o', linestyle='dashed')
+
+    ax1.set_ylabel("runtime [s]")
+    ax1.legend()
+
+    filename = prefix + "_images_distribution.pdf"
+
+    fig.savefig(filename, bbox_inches="tight")
+
 def plot(y, n_procs, prefix, type, plot_error=False):
     y_std, y_mean = take_mean(y)
 
