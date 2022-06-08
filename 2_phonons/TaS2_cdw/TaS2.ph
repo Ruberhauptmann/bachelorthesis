e257a2f4e6b1cc36564cdeebf1cdaf9fbfd8795f
tsievers <tsievers@physnet.uni-hamburg.de>
2022-06-08 15:33:55 +0200

    Some changes to TaS2 cdw ph input

--- 2_phonons/TaS2_cdw/TaS2.ph
+++ 2_phonons/TaS2_cdw/TaS2.ph
@@ -1,9 +1,9 @@
 TaS2
 &INPUTPH
     prefix = 'TaS2_cdw_phonons'
-    outdir   = '/fastscratch/tsievers/TaS2_cdw_phonons/'
+    outdir   = '/fastscratch/tsievers/TaS2_cdw_phonons__180/'
 
-    fildyn   = '/fastscratch/tsievers/TaS2_cdw_phonons/TaS2_cdw.dyn'
+    fildyn   = '/fastscratch/tsievers/TaS2_cdw_phonons_180/TaS2_cdw.dyn'
     fildvscf  = 'TaS2_cdw.dvscf'
 
     tr2_ph = 1e-21
--- 2_phonons/TaS2_cdw/TaS2_phonons.sh
+++ 2_phonons/TaS2_cdw/TaS2_phonons.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #$ -q infinix.q
 #$ -e $HOME/job_logs/error_logs/
-#$ -o /fastscratch/tsievers/TaS2_cdw_phonons/
-#$ -l h_cpu=48:00:00
+#$ -o /fastscratch/tsievers/TaS2_cdw_phonons_180/
+#$ -l h_cpu=150:00:00
 #$ -l h_vmem=3G
+#$ -l excl=TRUE
 #$ -pe mpi 180
-#$ -N TaS2_phonons
 #$ -cwd
 #$ -S /bin/bash
+#$ -N TaS2_cdw_Gamma
 #$ -M tsievers@physnet.uni-hamburg.de -m as
 
 QEPREFIX=/fastscratch/tsievers/qe-7.0-intel-compiler
 
 module load intel/oneAPI-2021.4
 
-mpirun $QEPREFIX/bin/pw.x -nk 10 -i TaS2_cdw.scf
+mpirun $QEPREFIX/bin/pw.x -nd 9 -nk 10 -i TaS2_cdw.scf
 
-/usr/bin/time --format=%e -p mpirun $QEPREFIX/bin/ph.x -nk 10 -i TaS2.ph
+mpirun $QEPREFIX/bin/ph.x -nd 9 -nk 10 -i TaS2.ph
