30181633ba1d3d5fcccc75a3c902fba40170d682
Ruberhauptmann <tjark.sievers@outlook.de>
2022-06-11 21:05:21 +0200

    outputs TaS2 nprocs intel

--- 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/send_jobs.py
+++ 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/send_jobs.py
@@ -36,7 +36,7 @@ def main():
     for run in range(1):
         out_directory = "out_files_TaS2/" + str(run) + "/singlecore"
         out_directories.append(out_directory)
-        job = "mpirun -np 1 pw.x -nk 1 < silicon.scf > " + out_directory + "/silicon_bench_singlecore_" + str(run) + ".out"
+        job = "mpirun -np 1 pw.x -nk 1 < silicon.scf > " + out_directory + "/silicon_bench_singlecore_" + str(run) + ".log"
         jobs.append(job)
         for poolsize in [2, 8]:
             out_directory = "out_files_TaS2/" + str(run) + "/" + str(poolsize)
@@ -44,7 +44,7 @@ def main():
             for n_procs in range(8, 97, 16):
                 if n_procs % poolsize == 0.:
                     nk = str(int(n_procs / poolsize))
-                    job = "mpirun -np " + str(n_procs) + " pw.x -nk " + nk + " -nd 1 < TaS2_cdw.scf > " + str(out_directory) + "/" + "TaS2_bench_poolsize_" + str(poolsize) + "_n_procs_" + str(n_procs) + "_" + str(run) + ".out"
+                    job = "mpirun -np " + str(n_procs) + " pw.x -nk " + nk + " -nd 1 < TaS2_cdw.scf > " + str(out_directory) + "/" + "TaS2_bench_poolsize_" + str(poolsize) + "_n_procs_" + str(n_procs) + "_" + str(run) + ".log"
 
                     jobs.append(job)
 
@@ -54,7 +54,7 @@ def main():
             for n_procs in range(18, 97, 18):
                 if n_procs % poolsize == 0.:
                     nk = str(int(n_procs / poolsize))
-                    job = "mpirun -np " + str(n_procs) + " pw.x -nk " + nk + " -nd 1 < TaS2_cdw.scf > " + str(out_directory) + "/" + "TaS2_bench_poolsize_" + str(poolsize) + "_n_procs_" + str(n_procs) + "_" + str(run) + ".out"
+                    job = "mpirun -np " + str(n_procs) + " pw.x -nk " + nk + " -nd 1 < TaS2_cdw.scf > " + str(out_directory) + "/" + "TaS2_bench_poolsize_" + str(poolsize) + "_n_procs_" + str(n_procs) + "_" + str(run) + ".log"
 
                     jobs.append(job)
 
--- 1_scf/silicon_benchmarks/si_bench_nk_intel_compiler_const_poolsize_hlrn/send_jobs.py
+++ 1_scf/silicon_benchmarks/si_bench_nk_intel_compiler_const_poolsize_hlrn/send_jobs.py
@@ -36,7 +36,7 @@ def main():
     for run in range(1):
         out_directory = "out_file_silicon/" + str(run) + "/singlecore"
         out_directories.append(out_directory)
-        job = "mpirun -np 1 pw.x -nk 1 < silicon.scf > " + out_directory + "/silicon_bench_singlecore_" + str(run) + ".out"
+        job = "mpirun -np 1 pw.x -nk 1 < silicon.scf > " + out_directory + "/silicon_bench_singlecore_" + str(run) + ".log"
         jobs.append(job)
         for poolsize in [2, 8, 16]:
             out_directory = "out_files_silicon/" + str(run) + "/" + str(poolsize)
@@ -44,7 +44,7 @@ def main():
             for n_procs in range(8, 81, 8):
                 if n_procs % poolsize == 0.:
                     nk = str(int(n_procs / poolsize))
-                    job = "mpirun -np " + str(n_procs) + " pw.x -nk " + nk + " -nd 1 < silicon.scf > " + out_directory + "/" + "silicon_bench_nk_" + nk + "_n_procs_" + str(n_procs) + "_" + str(run) + ".out"
+                    job = "mpirun -np " + str(n_procs) + " pw.x -nk " + nk + " -nd 1 < silicon.scf > " + out_directory + "/" + "silicon_bench_nk_" + nk + "_n_procs_" + str(n_procs) + "_" + str(run) + ".log"
 
                     jobs.append(job)
 
