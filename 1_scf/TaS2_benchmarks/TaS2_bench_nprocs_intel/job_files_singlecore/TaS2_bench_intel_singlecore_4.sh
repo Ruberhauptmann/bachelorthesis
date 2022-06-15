0712fe9dada189ab874aead46a760ee19ecb421f
tsievers <tsievers@physnet.uni-hamburg.de>
2022-06-15 19:58:44 +0200

    Inputs TaS2 singlecore times

---
+++ 1_scf/TaS2_benchmarks/TaS2_bench_nprocs_intel/job_files_singlecore/TaS2_bench_intel_singlecore_4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -q th1prio.q
+#$ -o /afs/physnet.uni-hamburg.de/users/th1_we/tsievers/job_logs/TaS2/bench_nprocs_intel/singlecore/4
+#$ -pe mpi 4
+#$ -N TaS2_bench_intel_singlecore_4
+#$ -l h_cpu=25:00:00
+#$ -l h_vmem=20G
+#$ -l s_vmem=15G
+#$ -l excl=TRUE
+#$ -cwd
+#$ -S /bin/bash
+#$ -M tsievers@physnet.uni-hamburg.de -m as
+
+QEPREFIX=/fastscratch/tsievers/qe-7.0-intel-compiler
+
+module load intel/oneAPI-2021.4
+
+/usr/bin/time --format=%e -p mpirun -n 1 $QEPREFIX/bin/pw.x -i in_files_singlecore/TaS2_bench_intel_singlecore_4.scf
