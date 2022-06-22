4fb694fd1382bed4b509fa35e707464fa8b0b466
Ruberhauptmann <tjark.sievers@outlook.de>
2022-06-22 11:33:09 +0200

    Add some more HLRN output files

--- 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/TaS2_nk_benchmark_hlrn.sh
+++ 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/TaS2_nk_benchmark_hlrn.sh
@@ -13,38 +13,38 @@ export SLURM_CPU_BIND=none
 cd $SLURM_SUBMIT_DIR
 
 
-mpirun -np 4 pw.x -nk 1 < TaS2_cdw.scf > out_files_TaS2/1/singlecore/TaS2_bench_singlecore_1.log
+mpirun -np 4 pw.x -nk 1 < TaS2_cdw.scf > out_files_TaS2/2/singlecore/TaS2_bench_singlecore_2.log
 
-mpirun -np 8 pw.x -nk 4 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/2/TaS2_bench_poolsize_2_n_procs_8_1.log
+mpirun -np 8 pw.x -nk 4 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/2/TaS2_bench_poolsize_2_n_procs_8_2.log
 
-mpirun -np 24 pw.x -nk 12 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/2/TaS2_bench_poolsize_2_n_procs_24_1.log
+mpirun -np 24 pw.x -nk 12 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/2/TaS2_bench_poolsize_2_n_procs_24_2.log
 
-mpirun -np 40 pw.x -nk 20 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/2/TaS2_bench_poolsize_2_n_procs_40_1.log
+mpirun -np 40 pw.x -nk 20 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/2/TaS2_bench_poolsize_2_n_procs_40_2.log
 
-mpirun -np 56 pw.x -nk 28 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/2/TaS2_bench_poolsize_2_n_procs_56_1.log
+mpirun -np 56 pw.x -nk 28 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/2/TaS2_bench_poolsize_2_n_procs_56_2.log
 
-mpirun -np 72 pw.x -nk 36 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/2/TaS2_bench_poolsize_2_n_procs_72_1.log
+mpirun -np 72 pw.x -nk 36 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/2/TaS2_bench_poolsize_2_n_procs_72_2.log
 
-mpirun -np 88 pw.x -nk 44 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/2/TaS2_bench_poolsize_2_n_procs_88_1.log
+mpirun -np 88 pw.x -nk 44 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/2/TaS2_bench_poolsize_2_n_procs_88_2.log
 
-mpirun -np 8 pw.x -nk 1 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/8/TaS2_bench_poolsize_8_n_procs_8_1.log
+mpirun -np 8 pw.x -nk 1 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/8/TaS2_bench_poolsize_8_n_procs_8_2.log
 
-mpirun -np 24 pw.x -nk 3 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/8/TaS2_bench_poolsize_8_n_procs_24_1.log
+mpirun -np 24 pw.x -nk 3 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/8/TaS2_bench_poolsize_8_n_procs_24_2.log
 
-mpirun -np 40 pw.x -nk 5 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/8/TaS2_bench_poolsize_8_n_procs_40_1.log
+mpirun -np 40 pw.x -nk 5 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/8/TaS2_bench_poolsize_8_n_procs_40_2.log
 
-mpirun -np 56 pw.x -nk 7 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/8/TaS2_bench_poolsize_8_n_procs_56_1.log
+mpirun -np 56 pw.x -nk 7 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/8/TaS2_bench_poolsize_8_n_procs_56_2.log
 
-mpirun -np 72 pw.x -nk 9 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/8/TaS2_bench_poolsize_8_n_procs_72_1.log
+mpirun -np 72 pw.x -nk 9 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/8/TaS2_bench_poolsize_8_n_procs_72_2.log
 
-mpirun -np 88 pw.x -nk 11 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/8/TaS2_bench_poolsize_8_n_procs_88_1.log
+mpirun -np 88 pw.x -nk 11 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/8/TaS2_bench_poolsize_8_n_procs_88_2.log
 
-mpirun -np 18 pw.x -nk 1 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/18/TaS2_bench_poolsize_18_n_procs_18_1.log
+mpirun -np 18 pw.x -nk 1 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/18/TaS2_bench_poolsize_18_n_procs_18_2.log
 
-mpirun -np 36 pw.x -nk 2 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/18/TaS2_bench_poolsize_18_n_procs_36_1.log
+mpirun -np 36 pw.x -nk 2 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/18/TaS2_bench_poolsize_18_n_procs_36_2.log
 
-mpirun -np 54 pw.x -nk 3 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/18/TaS2_bench_poolsize_18_n_procs_54_1.log
+mpirun -np 54 pw.x -nk 3 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/18/TaS2_bench_poolsize_18_n_procs_54_2.log
 
-mpirun -np 72 pw.x -nk 4 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/18/TaS2_bench_poolsize_18_n_procs_72_1.log
+mpirun -np 72 pw.x -nk 4 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/18/TaS2_bench_poolsize_18_n_procs_72_2.log
 
-mpirun -np 90 pw.x -nk 5 -nd 1 < TaS2_cdw.scf > out_files_TaS2/1/18/TaS2_bench_poolsize_18_n_procs_90_1.log
+mpirun -np 90 pw.x -nk 5 -nd 1 < TaS2_cdw.scf > out_files_TaS2/2/18/TaS2_bench_poolsize_18_n_procs_90_2.log
--- 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/submit_job.sh
+++ 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/submit_job.sh
@@ -1,11 +1,11 @@
 
-mkdir -p out_files_TaS2/1/singlecore
+mkdir -p out_files_TaS2/2/singlecore
 
-mkdir -p out_files_TaS2/1/2
+mkdir -p out_files_TaS2/2/2
 
-mkdir -p out_files_TaS2/1/8
+mkdir -p out_files_TaS2/2/8
 
-mkdir -p out_files_TaS2/1/18
+mkdir -p out_files_TaS2/2/18
 
 
 sbatch TaS2_nk_benchmark_hlrn.sh
