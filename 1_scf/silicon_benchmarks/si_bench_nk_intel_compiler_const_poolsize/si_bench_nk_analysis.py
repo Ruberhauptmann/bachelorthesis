1dec9ca916a21d7b13c18926d00050ca00b17925
Ruberhauptmann <tjark.sievers@outlook.de>
2022-06-26 21:59:13 +0200

    Added some runs

--- 1_scf/silicon_benchmarks/si_bench_nk_intel_compiler_const_poolsize/si_bench_nk_analysis.py
+++ 1_scf/silicon_benchmarks/si_bench_nk_intel_compiler_const_poolsize/si_bench_nk_analysis.py
@@ -1,6 +1,48 @@
 import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-colorblind')
 
 from qe_benchmarking import qe_helper, nk_plots
+
+def plot(y, n_procs, prefix, type, plot_error=False):
+    y_std, y_mean = nk_plots.take_mean(y)
+
+    fig, ax1 = plt.subplots()
+
+    for nk in n_procs:
+        if plot_error:
+            ax1.fill_between(n_procs[nk][0], y_mean[nk]-y_std[nk], y_mean[nk]+y_std[nk], alpha=0.2)
+            ax1.plot(n_procs[nk][0], y_mean[nk], label=nk, marker='o', linestyle='dashed')
+        else:
+            ax1.plot(n_procs[nk][0], y_mean[nk], label=nk, marker='o', linestyle='dashed')
+
+    max_nprocs = 0
+    for nk in n_procs:
+        if np.max(n_procs[nk].flatten()) > max_nprocs:
+            max_nprocs = np.max(n_procs[nk].flatten())
+    
+    if type == "speedup":
+        linear_nprocs = np.linspace(0, max_nprocs)
+        ax1.plot(linear_nprocs, linear_nprocs)
+
+    ax1.set_xlabel("Number of processors")
+
+    if type == "absolute":
+        ax1.set_ylabel("runtime [s]")
+        ax1.axhline(y=25, color='orange', linestyle='dashed')
+    if type == "speedup":
+        ax1.axhline(y=1, color='r', linestyle='dashed')
+        ax1.set_ylabel("speedup")
+    if type == "wait":
+        ax1.set_ylabel("system time")
+    if type == "efficiency":
+        ax1.set_ylabel("efficiency (speedup / Number of processors)")
+
+    ax1.legend()
+
+    filename = prefix + "_bench_nk_" + type + ".pdf"
+
+    fig.savefig(filename, bbox_inches="tight")
 
 if __name__ == "__main__":
     ### Plot absolute times
@@ -13,7 +55,9 @@ if __name__ == "__main__":
     print("Minimum time: ", minimum_time)
     print("With processors: ", int(n_procs["2"][0][np.where(walltimes_mean["2"] == minimum_time)][0]))
 
-    nk_plots.plot(walltimes, n_procs, "si_intel", "absolute", plot_error=True)
+    print("Time at ", n_procs["2"][0][4], " processors: ", walltimes_mean["2"][4])
+
+    plot(walltimes, n_procs, "si_intel", "absolute", plot_error=True)
 
     ### Plot speedup
 
