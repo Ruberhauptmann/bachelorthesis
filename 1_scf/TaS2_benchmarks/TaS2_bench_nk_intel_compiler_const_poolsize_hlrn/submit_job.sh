5b22a45e5ec9020a09de96150dab3c1a91d9d1f7
Ruberhauptmann <tjark.sievers@outlook.de>
2022-06-26 10:03:40 +0200

    Some thesis work

--- 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/submit_job.sh
+++ 1_scf/TaS2_benchmarks/TaS2_bench_nk_intel_compiler_const_poolsize_hlrn/submit_job.sh
@@ -1,4 +1,8 @@
 
+mkdir -p out_files_TaS2/0/2
+
+mkdir -p out_files_TaS2/0/8
+
 mkdir -p out_files_TaS2/0/24
 
 mkdir -p out_files_TaS2/0/36
@@ -6,6 +10,12 @@ mkdir -p out_files_TaS2/0/36
 mkdir -p out_files_TaS2/0/48
 
 mkdir -p out_files_TaS2/0/72
+
+mkdir -p out_files_TaS2/0/18
+
+mkdir -p out_files_TaS2/1/2
+
+mkdir -p out_files_TaS2/1/8
 
 mkdir -p out_files_TaS2/1/24
 
@@ -15,6 +25,12 @@ mkdir -p out_files_TaS2/1/48
 
 mkdir -p out_files_TaS2/1/72
 
+mkdir -p out_files_TaS2/1/18
+
+mkdir -p out_files_TaS2/2/2
+
+mkdir -p out_files_TaS2/2/8
+
 mkdir -p out_files_TaS2/2/24
 
 mkdir -p out_files_TaS2/2/36
@@ -22,6 +38,12 @@ mkdir -p out_files_TaS2/2/36
 mkdir -p out_files_TaS2/2/48
 
 mkdir -p out_files_TaS2/2/72
+
+mkdir -p out_files_TaS2/2/18
+
+mkdir -p out_files_TaS2/3/2
+
+mkdir -p out_files_TaS2/3/8
 
 mkdir -p out_files_TaS2/3/24
 
@@ -31,6 +53,12 @@ mkdir -p out_files_TaS2/3/48
 
 mkdir -p out_files_TaS2/3/72
 
+mkdir -p out_files_TaS2/3/18
+
+mkdir -p out_files_TaS2/4/2
+
+mkdir -p out_files_TaS2/4/8
+
 mkdir -p out_files_TaS2/4/24
 
 mkdir -p out_files_TaS2/4/36
@@ -38,6 +66,8 @@ mkdir -p out_files_TaS2/4/36
 mkdir -p out_files_TaS2/4/48
 
 mkdir -p out_files_TaS2/4/72
+
+mkdir -p out_files_TaS2/4/18
 
 
 sbatch TaS2_nk_benchmark_hlrn.sh
